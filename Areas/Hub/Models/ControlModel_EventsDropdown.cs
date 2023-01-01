953e92985cb862be5656d5a54d76f6970f801f82
Fohristiwhirl-Skaffen-Amtiskaw <112807626+Fohristiwhirl-Skaffen-Amtiskaw@users.noreply.github.com>
2023-01-01 03:46:19 +0000

    Changed Layout and Styling

---
+++ Areas/Hub/Models/ControlModel_EventsDropdown.cs
@@ -0,0 +1,12 @@
+﻿namespace LetsGame.Areas.Hub.Models
+{
+	public class ControlModel_EventsDropdown
+	{
+		public ControlModel_EventsDropdown(ControlModel_EventData e,ControlModel_EventData d) {
+			Edit = e;
+			Delete = d;
+		}
+		public ControlModel_EventData Edit { get; set; }
+		public ControlModel_EventData Delete { get; set; }
+	}
+}
